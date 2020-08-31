25d4e1978075d46e7244ca097fd87406f01c78b7
Thomas Carrillo <thomascarrillogonzalez@gmail.com>
2020-08-31 12:45:45 -0400

    Segundo Commit: Se agregan widget y se agregan refencias desde Java

--- app/src/main/java/com/srcarrillo/ministeriopuertaabiertalirqun/MainActivity.java
+++ app/src/main/java/com/srcarrillo/ministeriopuertaabiertalirqun/MainActivity.java
@@ -3,12 +3,26 @@ package com.srcarrillo.ministeriopuertaabiertalirqun;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtTitulo;
+    private Button btnIniciar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtTitulo = findViewById(R.id.txtTitulo);
+        btnIniciar = findViewById(R.id.btnIniciar);
+    }
+
+    public void Saludar(){
+
     }
 }
