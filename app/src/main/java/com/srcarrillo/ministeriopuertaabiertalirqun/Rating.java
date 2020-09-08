4c5c0d7a31b9bfca1df99dc8704935b16d662a5f
Thomas Carrillo <Thomascarrillogonzalez@gmail.com>
2020-09-07 22:57:53 -0300

    10 Commit: Se modifica interfaz visual buscando interfaz de app definitiva

--- app/src/main/java/com/srcarrillo/ministeriopuertaabiertalirqun/Rating.java
+++ app/src/main/java/com/srcarrillo/ministeriopuertaabiertalirqun/Rating.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,6 +20,7 @@ public class Rating extends AppCompatActivity {
     private EditText inputNombre, inputComentario;
     private Button btnRatingVolver, btnEnviar;
     private RatingBar rbAyudanos;
+    private boolean NombreOk = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +32,11 @@ public class Rating extends AppCompatActivity {
         btnRatingVolver = findViewById(R.id.btnRatingVolver);
         btnEnviar = findViewById(R.id.btnEnviar);
         rbAyudanos = findViewById(R.id.rbAyudanos);
+
+        btnEnviar.setEnabled(false);
+        rbAyudanos.setNumStars(7);
+        rbAyudanos.setEnabled(false);
+        rbAyudanos.setRating(0);
 
         btnRatingVolver.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,8 +51,20 @@ public class Rating extends AppCompatActivity {
                 enviarEmail(inputNombre.getText().toString()+":\n"+inputComentario.getText().toString()+"\n"+rbAyudanos.getRating()+" Estrellas");
             }
         });
+        inputNombre.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if (inputNombre.getText().toString().length()<3){
+                    inputNombre.setError("Nombre muy Corto ( 4 Caracteres mínimo )");
+                }else {
+                    NombreOk = true;
+                }
+                return false;
+            }
+        });
 
     }
+
     public void enviarEmail(String mensaje){
         Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
 
